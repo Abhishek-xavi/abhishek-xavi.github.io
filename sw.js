a220c90dd3a772358638c2a61a451fe539b04523
Abhishek Gupta <abhishek.gupta0724@gmail.com>
2021-10-04 11:55:16 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-390e854f6aea7ac12f15.js"
+    "url": "webpack-runtime-00d1f1dba26bad97d2a2.js"
   },
   {
     "url": "framework-305b3707783ccc9d7ca6.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "44d509862d4444bc3cdb5a8284e73373"
+    "revision": "107afb512bc3df207eeca477fc154e86"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-16703ee5599528db9f93.js"
